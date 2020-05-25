f7b2e11363c30e41e5346ab2ba9d17141dce35da
Leticia <leticiagabriellourenco@gmail.com>
2020-05-24 23:30:40 -0700

    Refactor view with basic bootstrap-react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 
@@ -8,7 +10,7 @@ class App extends Component {
         this.state = {
           fieldA: '',
           fieldB: ''
-        };
+        }
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmission = this.handleSubmission.bind(this);
@@ -24,39 +26,55 @@ class App extends Component {
                 return { fieldB: inputValue }
             }
         });
-    };
+    }
 
     handleSubmission = event => {
         event.preventDefault();
         this.setState((prevState, props) => {
             return { fieldA: '', fieldB: '' }
         });
-    };
+    }
 
   render() {
       const { fieldA, fieldB } = this.state;
 
+      let request_json = {
+          "sentence_one":"I am flying a car",
+          "sentence_two":"I am flying a helicopter"
+      }
+
+      let response_json = {
+          "sentence_meta": {
+          "angle": 42.93550485861645,
+              "levenshtein_distance": 8.0,
+              "same_bucket": true
+            }
+      }
+
       return (
-      <div className="kondo-playground">
-        kondo-playground
-        <form>
-          <input
-              type="text"
-              value={ fieldA }
-              onChange={ event => this.handleInputChange(event, "fieldA") }
-          />
-          <input
-              type="text"
-              value={ fieldB }
-              onChange={ event => this.handleInputChange(event, "fieldB") }
-          />
-          <button
-              onClick={ this.handleSubmission }
-          >
-            search
-          </button>
-        </form>
-      </div>
+      <Container fluid>
+          <Container>
+              <Row>
+                  <Col>
+                      <Form>
+                          <Form.Control
+                              type="text"
+                              value={ fieldA }
+                              onChange={ event => this.handleInputChange(event, "fieldA") }
+                          />
+                          <Form.Control
+                              type="text"
+                              value={ fieldB }
+                              onChange={ event => this.handleInputChange(event, "fieldB") }
+                          />
+                          <Button
+                              onClick={ this.handleSubmission }
+                          >Search</Button>
+                      </Form>
+                  </Col>
+              </Row>
+          </Container>
+      </Container>
     );
   }
 }
