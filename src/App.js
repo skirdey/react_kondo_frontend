91b1ba2ff559705e486ffe60157b93f3f2a0b56b
Leticia <leticiagabriellourenco@gmail.com>
2020-05-25 00:23:55 -0700

    Style app in quick-drag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         super(props);
         this.state = {
           fieldA: '',
-          fieldB: ''
+          fieldB: '',
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -38,39 +38,51 @@ class App extends Component {
   render() {
       const { fieldA, fieldB } = this.state;
 
-      let request_json = {
-          "sentence_one":"I am flying a car",
-          "sentence_two":"I am flying a helicopter"
-      }
-
-      let response_json = {
-          "sentence_meta": {
-          "angle": 42.93550485861645,
-              "levenshtein_distance": 8.0,
-              "same_bucket": true
-            }
-      }
-
       return (
       <Container fluid>
-          <Container>
+          <Container id="content">
               <Row>
-                  <Col>
+                  <Col md={{ span: 6, offset: 3 }} className="terminal">
+                      <h5>Kondo Playground</h5>
+                      <p>
+                          go ahead, enter two sentences and we'll run it through kondo api so you can see the magic happen
+                      </p>
                       <Form>
                           <Form.Control
                               type="text"
                               value={ fieldA }
                               onChange={ event => this.handleInputChange(event, "fieldA") }
+                              autocapitalize="off"
+                              autocomplete="off"
+                              spellcheck="false"
+                              autocorrect="off"
                           />
                           <Form.Control
                               type="text"
                               value={ fieldB }
                               onChange={ event => this.handleInputChange(event, "fieldB") }
+                              autocapitalize="off"
+                              autocomplete="off"
+                              spellcheck="false"
+                              autocorrect="off"
                           />
                           <Button
+                              block
+                              variant="outline-light"
                               onClick={ this.handleSubmission }
-                          >Search</Button>
+                          ><small>&#36;</small> run kondo search</Button>
                       </Form>
+                      <div className="response">
+                          <code>
+                              &#123;
+                              "sentence_meta"&#58; &#123;<br/>
+                              "angle"&#58; 42.93550485861645,<br/>
+                              "levenshtein_distance"&#58; 8.0,<br/>
+                              "same_bucket"&#58; true<br/>
+                              &#125;
+                              &#125;
+                          </code>
+                      </div>
                   </Col>
               </Row>
           </Container>
